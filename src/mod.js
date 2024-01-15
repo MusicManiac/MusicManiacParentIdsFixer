e8d6fd9d91c618060bf8f5c68d86b561bf7ff9df
MusicManiac <14209235+MusicManiac@users.noreply.github.com>
2024-01-15 21:32:09 +0100

    Added lasers

--- src/mod.js
+++ src/mod.js
@@ -10,9 +10,13 @@ class WeaponCategoriesFixer {
         const tables = db.getTables();
         // Get item database from tables
         let itemDB = tables.templates.items;
-        itemDB["628b5638ad252a16da6dd245"]._parent = "5447b5fc4bdc2d87278b4567"; // SAG AK-545
-        itemDB["628b9c37a733087d0d7fe84b"]._parent = "5447b5fc4bdc2d87278b4567"; // SAG AK-545 short
-        itemDB["5d43021ca4b9362eab4b5e25"]._parent = "5447b5fc4bdc2d87278b4567"; // Lone Star TX-15 DML 5.56x45 carbine
+        const assaultCarbineParentId = "5447b5fc4bdc2d87278b4567";
+        const assaultRifleParentId = "5447b5f14bdc2d61278b4567";
+        itemDB["628b5638ad252a16da6dd245"]._parent = assaultCarbineParentId; // SAG AK-545 5.45x39 carbine
+        itemDB["628b9c37a733087d0d7fe84b"]._parent = assaultCarbineParentId; // SAG AK-545 Short 5.45x39 carbine
+        itemDB["5d43021ca4b9362eab4b5e25"]._parent = assaultCarbineParentId; // Lone Star TX-15 DML 5.56x45 carbine
+        itemDB["57c44b372459772d2b39b8ce"]._parent = assaultRifleParentId; // AS VAL 9x39 special assault rifle
+        itemDB["59e6687d86f77411d949b251"]._parent = assaultCarbineParentId; // Molot VPO-209 .366 TKM carbine
         logger.info("[Weapon Categories Fixer] MusicManiac - Weapon Categories Fixer Loaded:");
     }
 }
--- src/mod.ts
+++ src/mod.ts
@@ -18,12 +18,17 @@ class WeaponCategoriesFixer implements IPostDBLoadMod
 
 		const assaultCarbineParentId = "5447b5fc4bdc2d87278b4567";
 		const assaultRifleParentId = "5447b5f14bdc2d61278b4567";
+		const lightTargetPointersParentID = "55818b0e4bdc2dde698b456e";
 
 		itemDB["628b5638ad252a16da6dd245"]._parent = assaultCarbineParentId; // SAG AK-545 5.45x39 carbine
 		itemDB["628b9c37a733087d0d7fe84b"]._parent = assaultCarbineParentId; // SAG AK-545 Short 5.45x39 carbine
 		itemDB["5d43021ca4b9362eab4b5e25"]._parent = assaultCarbineParentId; // Lone Star TX-15 DML 5.56x45 carbine
 		itemDB["57c44b372459772d2b39b8ce"]._parent = assaultRifleParentId; // AS VAL 9x39 special assault rifle
 		itemDB["59e6687d86f77411d949b251"]._parent = assaultCarbineParentId; // Molot VPO-209 .366 TKM carbine
+
+		itemDB["5c079ed60db834001a66b372"]._parent = lightTargetPointersParentID; // TT DLP Tactical Precision LAM-module
+		itemDB["5cc9c20cd7f00c001336c65d"]._parent = lightTargetPointersParentID; // NcSTAR Tactical blue laser LAM-module
+
 
 		logger.info("[Weapon Categories Fixer] MusicManiac - Weapon Categories Fixer Loaded:");
 	}
